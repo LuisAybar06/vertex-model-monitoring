09887a8db22c794ec1f1d170abebc4a59e4372b7
PC <aluisaybar@gmail.com>
2024-07-07 22:17:06 -0500

    test terraform

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "google" {
+    project = var.project_id
+    region  = var.region
+  }
+  
+  resource "google_bigquery_dataset" "dataset" {
+    dataset_id = "my_dataset"
+    project    = var.project_id
+    location   = var.region
+  }
+  
+  resource "google_bigquery_table" "table1" {
+    table_id   = "my_table1"
+    dataset_id = google_bigquery_dataset.dataset.dataset_id
+    project    = var.project_id
+  
+    schema = jsonencode([
+      {
+        name = "id"
+        type = "STRING"
+        mode = "REQUIRED"
+      },
+      {
+        name = "name"
+        type = "STRING"
+        mode = "NULLABLE"
+      }
+    ])
+  }
+  
+  resource "google_bigquery_table" "table2" {
+    table_id   = "my_table2"
+    dataset_id = google_bigquery_dataset.dataset.dataset_id
+    project    = var.project_id
+  
+    schema = jsonencode([
+      {
+        name = "id"
+        type = "STRING"
+        mode = "REQUIRED"
+      },
+      {
+        name = "value"
+        type = "FLOAT"
+        mode = "NULLABLE"
+      }
+    ])
+  }
+  
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "groovy-form-424500-f2"
+region     = "us-central1"
